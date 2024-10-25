002b1d45ffbcf007964dfe5d281dd105ea13f08f
Nerfer1205 <nile120501@hotmail.com>
2024-10-25 01:03:17 -0500

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,76 @@
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class ProductsService {
+
+  constructor(private prismaService: PrismaService) { }
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      return await this.prismaService.product.create({
+        data: createProductDto
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException(`Producto con nombre ${createProductDto.name} ya existe`);
+        }
+      }
+      console.error(error);
+      
+      throw new InternalServerErrorException();
+
+    }
+  }
+
+  findAll() {
+    return this.prismaService.product.findMany();
+  }
+
+  async findOne(id: number) {
+    const productFound = await this.prismaService.product.findUnique({
+      where: {
+        id: id
+      }
+    });
+
+    if (!productFound) {
+      throw new NotFoundException(`El producto de id ${id} no fue encontrado`);
+    }
+
+    return productFound;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const productFound = await this.prismaService.product.update({
+      where: {
+        id: id
+      },
+      data: updateProductDto
+    });
+
+    if (!productFound) {
+      throw new NotFoundException(`El producto de id ${id} no fue encontrado`);
+    }
+
+    return productFound;
+  }
+
+  async remove(id: number) {
+    const deletedProduct = await this.prismaService.product.delete({
+      where: {
+        id: id
+      }
+    });
+
+    if (!deletedProduct) {
+      throw new NotFoundException(`El producto de id ${id} no fue encontrado`);
+    }
+
+    return deletedProduct;
+  }
+}
